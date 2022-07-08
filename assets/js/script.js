600064273910eea99511cbeeb0a00f71005ed0a0
devSantos16 <devsantos1999@gmail.com>
2022-07-08 15:05:46 -0300

    Fix + Validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 button_add = document.getElementById("button_add");
 button_add.addEventListener("click", adicionarTarefa);
 
+var array = [];
+
 const ElementosHTML = {
     item_event: document.querySelector(".item-event"),
     content_item_event: "content-item-event"
@@ -8,6 +10,15 @@ const ElementosHTML = {
 
 function adicionarTarefa() {
     var texto = document.getElementById("to-do-input").value;
+    // zerar o valor
+    document.getElementById("to-do-input").value = "";
+
+    // verificar se ele é nulo
+    if (texto == "") {
+        window.alert("Não tem nada na tarefa");
+        return;
+    }
+    // verificar se ja existe a tarefa
     for (var i = 0; i < array.length; i++) {
         if (array[i] == texto) {
             window.alert("Você não pode adicionar a mesma tarefa");
@@ -19,7 +30,9 @@ function adicionarTarefa() {
 }
 
 function desenharElementoTela(texto) {
+    document.querySelector(".to-do-list-event").style.visibility  = "visible";
     var div_content_item_event = document.createElement("div");
+
     div_content_item_event.setAttribute("class", ElementosHTML.content_item_event);
     ElementosHTML.item_event.appendChild(div_content_item_event);
 
@@ -27,17 +40,21 @@ function desenharElementoTela(texto) {
 
     input.setAttribute("type", "checkbox");
     input.setAttribute("class", "checkbox");
+
     var tarefa = document.createElement("tarefa");
+
     tarefa.setAttribute("class", 'tarefa_event');
     tarefa.innerHTML = texto;
+
     div_content_item_event.appendChild(input);
     div_content_item_event.appendChild(tarefa);
 
-    input.addEventListener("change", function(){
-        if(input.checked){
+    // adiciona o evento de checkbox
+    input.addEventListener("change", function () {
+        if (input.checked) {
             tarefa.style.textDecoration = "line-through";
         }
-        else{
+        else {
             tarefa.style.textDecoration = "none";
         }
     });
