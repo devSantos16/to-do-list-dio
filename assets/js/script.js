8476fb4f07c82c1f87fa8a767cf77e5c554c4936
devSantos16 <devsantos1999@gmail.com>
2022-07-08 14:12:22 -0300

    Add Checkbox checked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,45 @@
 button_add = document.getElementById("button_add");
 button_add.addEventListener("click", adicionarTarefa);
 
-var elementos = {
+const ElementosHTML = {
     item_event: document.querySelector(".item-event"),
     content_item_event: "content-item-event"
 };
 
-function adicionarTarefa(){
+function adicionarTarefa() {
     var texto = document.getElementById("to-do-input").value;
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] == texto) {
+            window.alert("Você não pode adicionar a mesma tarefa");
+            return;
+        }
+    }
+    array.push(texto);
     desenharElementoTela(texto);
 }
 
-function desenharElementoTela(texto){
+function desenharElementoTela(texto) {
     var div_content_item_event = document.createElement("div");
-    div_content_item_event.setAttribute("class", elementos.content_item_event);
-    elementos.item_event.appendChild(div_content_item_event);
+    div_content_item_event.setAttribute("class", ElementosHTML.content_item_event);
+    ElementosHTML.item_event.appendChild(div_content_item_event);
 
     var input = document.createElement("input");
-    input.setAttribute("type", "checkbox");
 
-    label = document.createElement("label").innerText = texto;
-        
+    input.setAttribute("type", "checkbox");
+    input.setAttribute("class", "checkbox");
+    var tarefa = document.createElement("tarefa");
+    tarefa.setAttribute("class", 'tarefa_event');
+    tarefa.innerHTML = texto;
     div_content_item_event.appendChild(input);
-    div_content_item_event.append(label);
-    
+    div_content_item_event.appendChild(tarefa);
+
+    input.addEventListener("change", function(){
+        if(input.checked){
+            tarefa.style.textDecoration = "line-through";
+        }
+        else{
+            tarefa.style.textDecoration = "none";
+        }
+    });
 
 }
-
-
